33893924815fbfcb2f1f34c50ca317bca4d4f405
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2024-01-25 20:17:14 +0100

    fix: gallery - add prevention of showing slide change on fullscreen mode when slide was changed before
    - fix code formatting

--- src/app/components/product-image-gallery/product-image-gallery.component.ts
+++ src/app/components/product-image-gallery/product-image-gallery.component.ts
@@ -3,13 +3,13 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  effect,
   ElementRef,
   EventEmitter,
   Input,
   Output,
   signal,
-  ViewChild,
-  ViewEncapsulation
+  ViewChild
 } from '@angular/core';
 import {SwiperContainer} from 'swiper/swiper-element';
 import {SwiperOptions} from 'swiper/types';
@@ -33,15 +33,17 @@ import {BreakpointsService} from '../../services/breakpoints.service';
 export class ProductImageGalleryComponent {
 
   @Input({required: true}) images!: {normal: string[]; thumb: string[]};
-  @Output() activeIndexChange = new EventEmitter<number>();
   @Output() clickNormal = new EventEmitter<void>();
+
   @ViewChild('appSwiper') protected appSwiper!: ElementRef<SwiperContainer>;
   @ViewChild('appSwiperThumbs') protected appSwiperThumbs!: ElementRef<SwiperContainer>;
-  @ViewChild('swiperButtonPrev') protected swiperButtonPrev!: ElementRef<HTMLDivElement>;
-  @ViewChild('swiperButtonNext') protected swiperButtonNext!: ElementRef<HTMLDivElement>;
+
+  private _firstMove = true;
+
   protected swiperConfig: SwiperOptions = {
     spaceBetween: 10
   }
+
   protected swiperThumbsConfig: SwiperOptions = {
     spaceBetween: 30,
     slidesPerView: 4,
@@ -49,51 +51,90 @@ export class ProductImageGalleryComponent {
     watchSlidesProgress: false,
     slideToClickedSlide: false
   }
-  protected isOnSmallAndBellow = computed(() => {
-    const currentScreenSizes = this._breakpointsService.currentScreenSizes();
-    return !currentScreenSizes.find((currentScreenSize) => currentScreenSize === 'medium');
-  });
 
   protected isOnExtraSmallAndBellow = computed(() => {
     const currentScreenSizes = this._breakpointsService.currentScreenSizes();
     return !currentScreenSizes.find((currentScreenSize) => currentScreenSize === 'small');
   });
 
-  constructor(
-    private _breakpointsService: BreakpointsService
-  ) {
-  }
+  protected isOnSmallAndBellow = computed(() => {
+    const currentScreenSizes = this._breakpointsService.currentScreenSizes();
+    return !currentScreenSizes.find((currentScreenSize) => currentScreenSize === 'medium');
+  });
 
   protected _activeIndex = signal(0);
 
   @Input() set activeIndex(activeIndex: number) {
-    setTimeout(() => {
-      this.appSwiper.nativeElement.swiper.slideTo(activeIndex, 0);
-      this.appSwiperThumbs.nativeElement.swiper.slideTo(activeIndex, 0);
+
+    if (activeIndex === this._activeIndex()) {
+      return;
+    }
+
+    if (!this._firstMove) {
+      this.appSwiper.nativeElement.swiper.slideTo(activeIndex);
+    } else {
       this._activeIndex.set(activeIndex);
-    });
+    }
   }
 
-  protected handleSlideChange(activeIndex: number) {
+  @Output() activeIndexChange = new EventEmitter<number>();
 
-    const swiperActiveIndex = activeIndex;
-    const swiperThumbsActiveIndex = this.appSwiperThumbs.nativeElement.swiper.activeIndex;
-    const slidesPerView = this.swiperThumbsConfig.slidesPerView as number;
+  private _activeIndexThumb = 0;
 
-    // edge
+  get activeIndexThumb() {
+    return this._activeIndexThumb;
+  }
 
-    const view = (slidesPerView - swiperActiveIndex) + swiperThumbsActiveIndex;
+  @Input() set activeIndexThumb(activeIndexThumb: number) {
 
-    if (view === 1) {
-      this.appSwiperThumbs.nativeElement.swiper.slideNext();
+    if (activeIndexThumb === this._activeIndexThumb) {
+      return;
     }
 
-    if (view === slidesPerView) {
-      this.appSwiperThumbs.nativeElement.swiper.slidePrev();
-    }
+    this._activeIndexThumb = activeIndexThumb;
+  }
 
-    this._activeIndex.set(activeIndex);
-    this.activeIndexChange.emit(activeIndex);
+  @Output() activeIndexThumbChange = new EventEmitter<number>();
+
+  constructor(
+    private _breakpointsService: BreakpointsService
+  ) {
+
+    effect(() => {
+
+      // thumb edge
+
+      const activeIndex = this._activeIndex();
+
+      if (!this.appSwiperThumbs) {
+        return;
+      }
+
+      if (this._firstMove) {
+        this.appSwiper.nativeElement.swiper.slideTo(activeIndex, 0);
+        this.appSwiperThumbs.nativeElement.swiper.slideTo(this.activeIndexThumb, 0);
+        this._firstMove = false;
+        return;
+      }
+
+      const activeIndexThumb = this.activeIndexThumb;
+      const slidesPerView = this.swiperThumbsConfig.slidesPerView as number;
+      const view = (slidesPerView - activeIndex) + activeIndexThumb;
+
+      if (view <= 1) {
+        const newSwiperThumbsActiveIndex = Math.abs(view - 2) + activeIndexThumb;
+        this.appSwiperThumbs.nativeElement.swiper.slideTo(newSwiperThumbsActiveIndex);
+        this.activeIndexThumb = newSwiperThumbsActiveIndex;
+        this.activeIndexThumbChange.emit(newSwiperThumbsActiveIndex);
+      }
+      if (view >= slidesPerView) {
+        let newSwiperThumbsActiveIndex = activeIndexThumb - Math.abs(view - (slidesPerView - 1));
+        newSwiperThumbsActiveIndex = newSwiperThumbsActiveIndex < 0 ? 0 : newSwiperThumbsActiveIndex
+        this.appSwiperThumbs.nativeElement.swiper.slideTo(newSwiperThumbsActiveIndex);
+        this.activeIndexThumb = newSwiperThumbsActiveIndex;
+        this.activeIndexThumbChange.emit(newSwiperThumbsActiveIndex);
+      }
+    }, {allowSignalWrites: true});
   }
 
   handleKeydownOnSwiperButtonPrev($event: KeyboardEvent) {
@@ -105,6 +146,23 @@ export class ProductImageGalleryComponent {
   handleKeydownOnSwiperButtonNext($event: KeyboardEvent) {
     if ($event.code === 'Enter') {
       this.appSwiper.nativeElement.swiper.slideNext();
+    }
+  }
+
+  handleAppSwiperSlideChange(index: number) {
+    this._activeIndex.set(index);
+    this.activeIndexChange.emit(index);
+  }
+
+  handleSwiperSlideThumbClick(index: number) {
+    this.appSwiper.nativeElement.swiper.slideTo(index);
+    this.activeIndexChange.emit(index);
+  }
+
+  handleSwiperSlideThumbKeydown($event: KeyboardEvent, index: number) {
+    if ($event.code === 'Enter') {
+      this.appSwiper.nativeElement.swiper.slideTo(index);
+      this.activeIndexChange.emit(index);
     }
   }
 }
